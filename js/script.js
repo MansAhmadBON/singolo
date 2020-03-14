fa6116e2d30a2585c29dc2ccfc764e3bd36de365
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-03-14 13:33:56 +0300

    feat: implemented features modalWindow 'Get a quote'

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 window.onload = function () {
     const navList = document.querySelector('.nav__list');
-
     const navLinks= document.querySelectorAll('.nav__link');
-
     const navLinkHove = document.querySelector('.nav__link-home');
     const navLinkServices = document.querySelector('.nav__link-services');
     const navLinkPortfolio = document.querySelector('.nav__link-portfolio');
     const navLinkAbout = document.querySelector('.nav__link-about');
     const navLinkContact = document.querySelector('.nav__link-contact');
 
-
     const portfolioBox = document.querySelector('.portfolio__columnsBox');
     const portfolioItems = document.querySelectorAll('.portfolio__item');
+
+    const form = document.querySelector('.quoteForm');
+    const inputFormSubject = document.querySelector('.quoteForm__input-subject');
+    const textAreaForm = document.querySelector('.quoteForm__textArea');
+    const btnFormSubmit = document.querySelector('.quoteForm__btn');
+    const modalWindow = document.querySelector('.modalWindowWrapper');
+    const modalWindowBtn = document.querySelector('.modalWindow__btn');
+    const modalWindowTarget = document.querySelector('.modalWindow__content');
 
 
     const removeNavActiveClass = () => navLinks.forEach(el => el.classList.remove('nav__link-active'));
     const addNavActiveClass = el => el.classList.add('nav__link-active');
 
-
-    const removePortfolioItemClass = () => portfolioItems.forEach(el => el.classList.remove('portfolio__item-active'))
+    const removePortfolioItemClass = () => portfolioItems.forEach(el => el.classList.remove('portfolio__item-active'));
 
     //Header
     navList.addEventListener('click', function (e) {
@@ -54,4 +58,36 @@ window.onload = function () {
         removePortfolioItemClass();
         if( elem.className.split(' ')[0] === 'portfolio__item') elem.classList.add('portfolio__item-active');
     });
+
+    //form
+    form.addEventListener('submit',e => e.preventDefault());
+
+    btnFormSubmit.addEventListener('click', function () {
+        const valueInpSubject = (inputFormSubject.value !== '') ? `Тема: ${inputFormSubject.value}` : 'Без темы';
+        const valueTextArea = (textAreaForm.value !== '') ? `Описание: ${textAreaForm.value}` : 'Без описания';
+
+        modalWindow.classList.add('modalWindowWrapper-active');
+
+        const modalWindowContent = document.createElement('div');
+        modalWindowContent.innerHTML = `
+                                       <h4 class="modalWindow__title">письмо отправлено</h4>
+                                       <p class="modalWindow__text modalWindow__valueInpSubject">${valueInpSubject}</p>
+                                       <p class="modalWindow__text modalWindow__valueTextArea">${valueTextArea}</p>
+                                    `;
+
+        modalWindowTarget.append(modalWindowContent);
+    });
+
+    modalWindowBtn.addEventListener('click', function (e) {
+        modalWindowTarget.innerHTML = '';
+        modalWindow.classList.remove('modalWindowWrapper-active');
+    });
+
+
+
+
+
+
+
+
 };
